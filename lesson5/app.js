0129387978301f276f606c86cbd317b2dcf28a67
Lyashko Sofia <dormouse002@gmail.com>
2019-03-16 12:09:34 +0200

    newLesson5

--- lesson5/app.js
+++ lesson5/app.js
@@ -1,10 +1,8 @@
-//Задание 1
-
 function getNumber() {
     let number = prompt('Введите число');
     if (isValidateNumber(number)) {
         alert('Неверно введено число');
-        number = getOperand()
+        number = getNumber()
     }
     return number;
 }
@@ -25,7 +23,7 @@ function sumOfEvenNumbers() {
     return sum;
 }
 
-alert('Сумма четных чисел ' + sumOfEvenNumbers());
+alert('Сумма четных чисел - ' + sumOfEvenNumbers());
 
 
 //задание 2
@@ -33,14 +31,14 @@ alert('Сумма четных чисел ' + sumOfEvenNumbers());
 let maxRandomNumber;
 
 function randomNumber(min, max) {
-    let random = min - 0.5 + Math.random() * (max - min + 1)
+    let random = min - 0.5 + Math.random() * (max - min + 1);
     random = Math.round(random);
     return random;
 }
 
 let numberOne = randomNumber(1000, 2000),
-    numberTwo = randomNumber(1000, 2000)
-console.log(numberOne, numberTwo)
+    numberTwo = randomNumber(1000, 2000);
+console.log(numberOne, numberTwo);
 
 maxRandomNumber = Math.max(numberOne, numberTwo);
 
