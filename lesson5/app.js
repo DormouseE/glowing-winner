5bd42ca6acdcc18a3262f5a9bff63410af4e27d1
Lyashko Sofia <dormouse002@gmail.com>
2019-03-16 11:46:34 +0200

    lesson5

--- lesson5/app.js
+++ lesson5/app.js
@@ -1,14 +1,49 @@
-function dad() {
-    let xc = prompt('vedite chislo')
-    if (!isValidateNumber(xc)) {
+//Задание 1
+
+function getOperand() {
+    var operand = prompt('Введите число');
+    if (isValidateNumber(operand)) {
         alert('Неверно введено число');
+        operand = getOperand()
     }
+    return operand;
 }
 
-
-
-function isValidateNumber(abz) {
-    return !(isNaN(abz)
+function isValidateNumber(value) {
+    return (isNaN(value)
         || value === ''
         || value === null);
 }
+
+function sumOfEvenNumbers() {
+    var str = getOperand()
+    var sum = 0;
+    for (var i = 0, len = str.length; i < len; i++) {
+        if (str[i] % 2 == 0)
+            sum++;
+    }
+    return sum;
+}
+
+alert('Сумма четных чисел ' + sumOfEvenNumbers());
+
+
+//задание 2
+
+let numberOne;
+let numberTwo;
+let maxRandomNumber;
+
+function randomNumber(min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1)
+    rand = Math.round(rand);
+    return rand;
+}
+
+numberOne = randomNumber(1000,2000)
+numberTwo = randomNumber(1000,2000)
+console.log(numberOne,numberTwo)
+
+maxRandomNumber = Math.max(numberOne, numberTwo);
+
+alert(maxRandom);
