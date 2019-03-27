3aea89b34794641f179e70bd14514cdd2b05b30c
Lyashko Sofia <dormouse002@gmail.com>
2019-03-27 23:13:48 +0200

    add

--- lesson8/app.js
+++ lesson8/app.js
@@ -2,22 +2,24 @@ const button = document.querySelector('button');
 const list = document.querySelector('ul');
 
 button.addEventListener('click', addItems);
-list.addEventListener('click', toggleBackgroundColor);
-list.addEventListener('click', removeElem);
+list.addEventListener('click', removeItems);
+list.addEventListener('click', switchBackgroundColorOfItems);
 
 function addItems() {
     let li = document.createElement('li');
     list.appendChild(li)
 }
 
-function toggleBackgroundColor(element) {
+function removeItems(element) {
+    if (element.target.tagName === 'LI')
+        if (element.altKey) element.target.remove();
+}
+
+
+function switchBackgroundColorOfItems(element) {
     if (element.target.tagName === 'LI')
         element.target.style.background = element.target.style.background === 'red' ? 'yellow' : 'red';
 }
 
 
-function removeElem(element) {
-    if (element.target.tagName === 'LI')
-        if (element.altKey) element.target.remove();
-}
 
