98f831baf92439b21ab2ffea6fe6e3038323b748
DormouseE <48258614+DormouseE@users.noreply.github.com>
2019-03-28 15:06:06 +0200

    Add files via upload

--- lesson8/app.js
+++ lesson8/app.js
@@ -1,25 +1,27 @@
+
 const button = document.querySelector('button');
 const list = document.querySelector('ul');
 
 button.addEventListener('click', addItem);
 list.addEventListener('click', removeItem);
-list.addEventListener('click', switchBackgroundColorOfItem);
+list.addEventListener('click', eventChangeBackgroundColor);
 
 function addItem() {
     let li = document.createElement('li');
     list.appendChild(li)
 }
 
-function removeItem (event) {
-    if (event.target.tagName === 'LI')
-        if (event.altKey) event.target.remove();
+function removeItem(event) {
+    if ((event.target.tagName === 'LI') && event.altKey) {
+        event.target.remove();
+    }
 }
 
-
-function switchBackgroundColorOfItem(event) {
-    if (event.target.tagName === 'LI')
-        event.target.style.background = event.target.style.background === 'red' ? 'yellow' : 'red';
+function eventChangeBackgroundColor(event) {
+    switchBackgroundColorOfItem(event.target);
 }
 
-
-
+function switchBackgroundColorOfItem(element) {
+    if (element.tagName === 'LI')
+        element.style.background = element.style.background === 'red' ? 'yellow' : 'red';
+}
