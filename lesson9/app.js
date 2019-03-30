bbaef8a2bfb51c01d85c8b75728aa8eff0edbf5e
DormouseE <48258614+DormouseE@users.noreply.github.com>
2019-03-30 19:26:46 +0200

    Add files via upload

--- lesson9/app.js
+++ lesson9/app.js
@@ -1,46 +1,53 @@
 const addContactBtn = document.getElementById('addContactBtn');
-const contactsList = document.getElementById('contactsList');
+const contactsList = document.getElementById('contactsList')
 const contactNameInput = document.getElementById('nameInput');
 const contactPhoneInput = document.getElementById('phoneInput');
 const contactAgeInput = document.getElementById('ageInput');
-const contactTemplate = document.getElementById('contactTemplate').innerHTML;
+const contactTemplete = document.getElementById('contactTemplete').innerHTML;
 
+
+contactsList.addEventListener('click', onDeleteContactBtnClick);
 addContactBtn.addEventListener('click', onAddContactBtnClick);
-addContact({name: 'Alex', phone:'111'});
 
-function onAddContactBtnClick(){
-    submitContact();
+
+
+function onAddContactBtnClick() {
+  submitContact();
 }
 
-function submitContact(){
-    const contact = {
-        name: contactNameInput.value,
-        phone: contactPhoneInput.value,
-        age: contactAgeInput.value,
-    }
-    addContact(contact);
-    resetContactForm();
+function onDeleteContactBtnClick() {
+  deleteContact(event.target);
 }
 
-function addContact(contact){
-    const contactTr = document.createElement('tr');
-    
-    contactTr.innerHTML = contactTemplate
-                .replace('{{name}}', contact.name)
-                .replace('{{phone}}', contact.phone)
-                .replace('{{age}}', contact.age || '-');
-
-    contactsList.appendChild(contactTr);
-
-}
-
-function resetContactForm(){
-    contactNameInput.value = '';
-    contactPhoneInput.value = '';
-    contactAgeInput.value = '';
-}
-
-function deleteContactRow(btn) {
-    const row = btn.parentNode.parentNode;
-    row.parentNode.removeChild(row);
+function submitContact() {
+  const contact = {
+    name: contactNameInput.value,
+    phone: contactPhoneInput.value,
+    age: contactAgeInput.value
   }
+  addContact(contact);
+  resetContactForm();
+}
+
+function deleteContact(e) {
+  if (e.id === "deleteContactBtn") {
+    e.parentNode.parentNode.remove();
+  }
+}
+
+function addContact(contact) {
+  const contactTr = document.createElement('tr');
+  contactTr.innerHTML = contactTemplete
+    .replace('{{name}}', contact.name)
+    .replace('{{phone}}', contact.phone)
+    .replace('{{age}}', contact.age);
+
+  contactsList.appendChild(contactTr);
+}
+
+function resetContactForm() {
+  contactNameInput.value = '';
+  contactPhoneInput.value = '';
+  contactAgeInput.value = '';
+}
+
