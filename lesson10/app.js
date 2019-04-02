478fd84a8b76511c5cb8006c8a903751c2075db7
Lyashko Sofia <dormouse002@gmail.com>
2019-04-02 20:15:01 +0300

    add

---
+++ lesson10/app.js
@@ -0,0 +1,35 @@
+const value = calculator(10);
+
+function calculator(operand) {
+
+    return {
+        add: function add(value) {
+            return operand + value;
+        },
+        div: function div(value) {
+            return operand / value;
+        },
+        sub: function sub(value) {
+            return operand - value;
+        },
+        mult: function mult(value) {
+            return operand * value;
+        },
+        set: function set(value) {
+            return operand = value;
+        },
+        get: function get(value) {
+            return operand;
+        }
+    }
+}
+
+
+console.log(value.add(45)) // возвращает 55
+console.log(value.sub(45)) // возвращает -35
+console.log(value.divide(5)) // возвращает 2
+console.log(value.mult(5)) // возвращает 50
+console.log(value.set(100)) // устанавливает базовое значение в 100
+console.log(value.get()) // возвращает базовое значение (в данный момент 100)
+
+console.log(value.mult(5)) // возвращает 500
