7c4a2026037c7e757dd49a6abc591f888f1a275a
Lyashko Sofia <dormouse002@gmail.com>
2019-04-10 18:27:03 +0300

    burger

--- lesson12/app.js
+++ lesson12/app.js
@@ -1,40 +1,52 @@
 'use strict';
 
-class Hamburger {
 
-    constructor(size, stuffing) {
-      this.price = size.price + stuffing.price;
-      this.calories = size.calories + stuffing.calories;
-    }
-    
-    calculatePrice() {
-      return this.price;
-    }
-    
-    calculateCalories() {
-      return this.calories;
-    }
-    
-    addTopping(topping) {
-      this.price += topping.price;
-      this.calories += topping.calories;
-    }
-    
-    removeTopping(topping) {
-      this.price -= topping.price;
-      this.calories -= topping.calories;
-    }
-}
-
-Hamburger.SIZE_SMALL = {price : 50, calories : 20};
-Hamburger.SIZE_BIG = {price : 100, calories : 40};
-Hamburger.STUFFING_CHEESE = {price : 10, calories : 20};
-Hamburger.STUFFING_SALAD = {price : 20, calories : 5};
-Hamburger.STUFFING_POTATO = {price : 15, calories : 10};
-Hamburger.TOPPING_SPICE = {price : 15, calories : 0};
-Hamburger.TOPPING_MAYO = {price : 20, calories : 5};
+Hamburger.SIZE_SMALL = { price: 50, calories: 20 };
+Hamburger.SIZE_BIG = { price: 100, calories: 40 };
+Hamburger.STUFFING_CHEESE = { price: 10, calories: 20 };
+Hamburger.STUFFING_SALAD = { price: 20, calories: 5 };
+Hamburger.STUFFING_POTATO = { price: 15, calories: 10 };
+Hamburger.TOPPING_SPICE = { price: 15, calories: 0 };
+Hamburger.TOPPING_MAYO = { price: 20, calories: 5 };
 
 const hamburger = new Hamburger(Hamburger.SIZE_BIG, Hamburger.STUFFING_CHEESE);
+
+function Hamburger(size, stuffing) {
+    this.price = size.price + stuffing.price;
+    this.calories = size.calories + stuffing.calories;
+}
+
+// function calculatePrice() {
+//     return this.price;
+// }
+
+// function calculateCalories() {
+//     return this.calories;
+// }
+
+// function addTopping(topping) {
+//     this.price += topping.price;
+//     this.calories += topping.calories;
+// }
+
+// function removeTopping(topping) {
+//     this.price -= topping.price;
+//     this.calories -= topping.calories;
+// }
+Hamburger.prototype.calculatePrice = function () { return this.price; }
+Hamburger.prototype.calculateCalories = function () { return this.calories; };
+
+Hamburger.prototype.addTopping = function (topping) {
+this.price += topping.price;
+    this.calories += topping.calories
+};
+
+Hamburger.prototype.removeTopping = function (topping) {
+this.price -= topping.price;
+    this.calories -= topping.calories;
+};
+
+
 
 console.log("Calories: " + hamburger.calculateCalories());
 console.log("Price: " + hamburger.calculatePrice());
