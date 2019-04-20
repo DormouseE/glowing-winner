3d15fe09ae9eff8039360c8d94999b2419e6d2db
Lyashko Sofia <dormouse002@gmail.com>
2019-04-20 19:45:37 +0300

    album

---
+++ lesson15/app.js
@@ -0,0 +1,59 @@
+class Album {
+    static CONTAINER_CLASS = 'album-container';
+    static ITEM_CLASS = 'album-item'
+
+    constructor(el) {
+        this.el = el;
+        this.init();
+    }
+
+    init() {
+        this.applyClasses()
+        this.setEvent();
+        this.addMainItem();
+
+
+    }
+
+    applyClasses() {
+        this.el.classList.add(Album.CONTAINER_CLASS);
+        Array.prototype.forEach.call(this.el.children, (el) => {
+            el.classList.add(Album.ITEM_CLASS);
+        });
+    }
+
+    addMainItem() {
+        const mainLi = document.createElement('li');
+        mainLi.className = 'album--main-img';
+
+        const mainImg = document.createElement('img');
+        this.mainImg = mainImg;
+
+        mainLi.appendChild(this.mainImg);
+        this.el.appendChild(mainLi);
+
+    }
+
+    setEvent() {
+        this.el.addEventListener('mouseover', this.onShowImg.bind(this))
+    }
+
+    onShowImg(e) {
+        if (e.target.className == Album.ITEM_CLASS) {
+            this.duplicateImg(e.target.children[0]);
+        }
+    }
+
+    duplicateImg(el) {
+        this.mainImg.src = el.src;
+        this.mainImg.alt = el.alt;
+    }
+
+}
+
+
+const album = new Album(document.getElementById('container'));
+
+
+
+
