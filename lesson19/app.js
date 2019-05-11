3ad91e8bc2ba08d5ebb71c74d56b2ee6c371deeb
DormouseE <48258614+DormouseE@users.noreply.github.com>
2019-05-11 14:36:26 +0300

    Add files via upload

--- lesson19/app.js
+++ lesson19/app.js
@@ -13,7 +13,7 @@ let contacts = [];
 init();
 
 function init() {
-    
+
     addContactBtn.addEventListener('click', onAddContactBtnClick);
     contactsList.addEventListener('click', onContactsListClick);
 
@@ -39,68 +39,68 @@ function setContacts(data) {
 function renderContactsList(data) {
     console.log(data)
     contactsList.innerHTML = data.map((el) => {
-        return contactTemplate 
-        .replace('{{name}}', el.name)
-        .replace('{{surname}}', el.surname)
+        return contactTemplate
+            .replace('{{name}}', el.name)
+            .replace('{{surname}}', el.surname)
             .replace('{{phone}}', el.phone)
             .replace('{{email}}', el.email)
             .replace('{{id}}', el.id)
             .replace('{{class}}', el.isDone ? 'done' : '')
-        }).join('\n');
-    }
-    
-    
-    function onAddContactBtnClick(event) {
-        event.preventDefault();
+    }).join('\n');
+}
+
+
+function onAddContactBtnClick(event) {
+    event.preventDefault();
 
     submitContact();
 }
 
 function submitContact() {
     const contact = {
-        name: contactNameInput.value, 
-        phone: contactPhoneInput.value, 
-        surname: contactSurnameInput.value, 
-        email: contactEmailInput.value, 
-        isDone:false
+        name: contactNameInput.value,
+        phone: contactPhoneInput.value,
+        surname: contactSurnameInput.value,
+        email: contactEmailInput.value,
+        isDone: false
     };
-    
+
     addContact(contact).then(fetchContactsList)
 }
 
 
 function addContact(contacts) {
-   return fetch(CONTACTS_URL,{
-       method: 'POST',
-       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(contacts)
-   })
+    return fetch(CONTACTS_URL, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contacts)
+    })
 }
 
 
-function onContactsListClick(event){
+function onContactsListClick(event) {
     if (event.target.tagName === 'BUTTON') {
         deleteContact(event.target.parentNode.parentNode)
             .then(fetchContactsList);
 
-    }else if(event.target.parentNode.classList.contains('contacts')){
+    } else if (event.target.parentNode.classList.contains('contacts')) {
         toggleContactState(event.target.parentNode)
             .then(fetchContactsList);
     }
 }
 
 
-function toggleContactState(el){
+function toggleContactState(el) {
     const id = el.dataset.contactId;
-    const contact = contacts.find((el) => {return el.id == id});
+    const contact = contacts.find((el) => { return el.id == id });
 
     contact.isDone = !contact.isDone;
     console.log(id, contact);
 
-    
+
     return fetch(CONTACTS_URL + '/' + contact.id, {
         method: "PUT",
         headers: {
@@ -111,9 +111,9 @@ function toggleContactState(el){
     })
 }
 
-function deleteContact(el){
+function deleteContact(el) {
     const id = el.dataset.contactId;
-    const contact = contacts.find((el) => {return el.id == id});
+    const contact = contacts.find((el) => { return el.id == id });
 
     return fetch(CONTACTS_URL + '/' + contact.id, {
         method: "DELETE",
